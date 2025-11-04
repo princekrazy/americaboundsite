13309d56127e2a6fb2dfa867801863966fc515b9
princekrazy <kuvengaprince@gmail.com>
2025-11-03 19:23:11 -0600

    Switch BrowserRouter to HashRouter for GitHub Pages routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom"; // <-- changed BrowserRouter to HashRouter
 
 // Screens
 import HomeScreen from "./Screens/HomeScreen";
